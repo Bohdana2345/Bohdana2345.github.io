4e5f41e49166b22ae8f4074fe5ae487b91a3fa3b
Bohdana2345 <159618189+Bohdana2345@users.noreply.github.com>
2024-10-23 23:12:04 +0300

    Add files via upload

---
+++ pr_6_js/myLoc.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', getMyLocation)
+
+function getMyLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(displayLocation)
+    }
+    else{
+        alert("Oops, no geolocation support")
+    }
+}
+
+function displayLocation(position){
+    let latitude = position.coords.latitude
+    let longitude = position.coords.longitude
+    let div = document.getElementById('location')
+    div.innerHTML = 'You are at Latitude: ${latitude},   '
+
+}
+
+
