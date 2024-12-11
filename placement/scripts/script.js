d4e1131e2a79ff5c82c26d6cdd72ff5448087fca
Bohdana2345 <159618189+Bohdana2345@users.noreply.github.com>
2024-12-11 11:58:49 +0200

    Update script.js

--- placement/scripts/script.js
+++ placement/scripts/script.js
@@ -34,7 +34,7 @@ window.addEventListener('DOMContentLoaded', () => {
             if (screenWidth < 1000) {
                 homeSection.style.background = `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #FFFFFF 200%), url('../assets/img/home-bg.webp')`;
             } else {
-                homeSection.style.background = `url('/assets/img/home-bg.webp')`;
+                homeSection.style.background = `url('../assets/img/home-bg.webp')`;
             }
         }
         homeSection.style.backgroundSize = 'cover';
