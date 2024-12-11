117b75527b3fba1d74dea8f2c16d07cf589f5491
Bohdana2345 <159618189+Bohdana2345@users.noreply.github.com>
2024-12-11 19:54:13 +0200

    Update script.js

--- placement/scripts/script.js
+++ placement/scripts/script.js
@@ -35,9 +35,6 @@ function homeSectionScrollEffect() {
         }
     }
 
-    // if (scrollY < homeHeight) {
-    //     content.style.transform = `translateY(${-scrollY / 3}px)`;
-    // }
 }
 
 window.addEventListener('scroll', homeSectionScrollEffect);
